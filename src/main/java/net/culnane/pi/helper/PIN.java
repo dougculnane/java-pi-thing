6f3632e9ca52c0134991a3af083a6d7a447a9357
Doug Culnane <doug@culnane.net>
2022-12-30 16:03:14 +0100

    Format and link for PINs.

--- src/main/java/net/culnane/pi/helper/PIN.java
+++ src/main/java/net/culnane/pi/helper/PIN.java
@@ -6,11 +6,30 @@ import net.culnane.pi.exceptions.PinConfigurationException;
  * https://github.com/Pi4J/pi4j-example-components/blob/main/src/main/java/com/pi4j/catalog/components/helpers/PIN.java
  */
 public enum PIN {
-	SDA1(2), SCL1(2), D4(4), TXD(14), RXD(15), D17(17), PWM18(18), D27(27), D22(22), D23(23), D24(24), MOSI(10),
-	MISO(9), D25(25), D11(11), CEO(8), CE1(7), 
-	D5(5), // GPIO_05 https://pinout.xyz/pinout/pin29_gpio5#;
-	D6(6), D16(16), D26(26), D20(20), D21(21), 
-	PWM12(12),  // https://pinout.xyz/pinout/pin32_gpio12# 
+	SDA1(2), 
+	SCL1(2),
+	D4(4),
+	TXD(14),
+	RXD(15), 
+	D17(17),   // https://pinout.xyz/pinout/pin11_gpio17#
+	PWM18(18), 
+	D27(27),   // https://pinout.xyz/pinout/pin13_gpio27#
+	D22(22), 
+	D23(23), 
+	D24(24), 
+	MOSI(10),
+	MISO(9), 
+	D25(25), 
+	D11(11), 
+	CEO(8), 
+	CE1(7), 
+	D5(5),     // GPIO_05 https://pinout.xyz/pinout/pin29_gpio5#;
+	D6(6), 
+	D16(16),
+	D26(26),
+	D20(20),
+	D21(21), 
+	PWM12(12), // https://pinout.xyz/pinout/pin32_gpio12# 
 	PWM13(13),
 	PWM19(19);
 	
